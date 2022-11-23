1e3379aa5cfea413eaecad78c28e23f8ebfa24df
Aaron Raudales <78047697+AaronRaudales@users.noreply.github.com>
2022-11-23 13:51:36 -0600

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 import mysql from "promise-mysql";
 
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: 'us-cdbr-east-06.cleardb.net',
     database: 'heroku_834748982488d10',
     user: 'b94b81efa9bac6',
